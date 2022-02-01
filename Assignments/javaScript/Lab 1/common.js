0e18cb4b738f86f82d31b1ddd161a9d4c91d34c5
Pavel <paveld@cybage.com>
2022-02-01 16:26:19 +0530

    FET Assignments added

---
+++ Assignments/javaScript/Lab 1/common.js
@@ -0,0 +1,7 @@
+function addNumbers(headParam, bodyParam) {
+    const num1 = parseInt(headParam);
+    const num2 = parseInt(bodyParam);
+    const res = num1 + num2;
+    document.write("Addition of " + headParam + " and " + bodyParam + " is: ");
+    document.write(res)
+}
